0e545390157477f24ad1dcb431d4a1350b4a3089
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2023-11-12 10:20:05 -0600

    Small refactor

--- experiments/compose_fx/compose_fx.ml
+++ experiments/compose_fx/compose_fx.ml
@@ -14,26 +14,6 @@ let fresh_parse_ctx () : parse_ctx =
   let fresh_fora : fresh_fora = fun x -> ref (ForA (fresh_int (), x)) in
   { fresh_var; fresh_fora }
 
-let parse s =
-  let lexbuf = Lexer.from_string s in
-  let lex = Lexer.provider lexbuf in
-  let parse =
-    MenhirLib.Convert.Simplified.traditional2revised Parser.toplevel
-  in
-  let parse_ctx = fresh_parse_ctx () in
-  try
-    let parsed = parse lex parse_ctx in
-    Ok parsed
-  with
-  | Lexer.SyntaxError what ->
-      Error
-        (Printf.sprintf "Syntax error: %s at %s" what
-           (string_of_position (Lexer.position lexbuf)))
-  | Parser.Error ->
-      Error
-        (Printf.sprintf "Parse error at %s"
-           (string_of_position (Lexer.position lexbuf)))
-
 let solve _p = failwith "todo"
 let lower _ctx _p = failwith "todo"
 let eval (_ty, _program) = failwith "todo"
@@ -47,7 +27,26 @@ module Compose_fx : LANGUAGE = struct
   type ir_program = unit
   type evaled_program = unit
 
-  let parse = parse
+  let parse s =
+    let lexbuf = Lexer.from_string s in
+    let lex = Lexer.provider lexbuf in
+    let parse =
+      MenhirLib.Convert.Simplified.traditional2revised Parser.toplevel
+    in
+    let parse_ctx = fresh_parse_ctx () in
+    try
+      let parsed = parse lex parse_ctx in
+      Ok parsed
+    with
+    | Lexer.SyntaxError what ->
+        Error
+          (Printf.sprintf "Syntax error: %s at %s" what
+             (string_of_position (Lexer.position lexbuf)))
+    | Parser.Error ->
+        Error
+          (Printf.sprintf "Parse error at %s"
+             (string_of_position (Lexer.position lexbuf)))
+
   let solve _p = failwith "todo"
   let ir _ = failwith "todo"
   let eval _ = failwith "todo"
